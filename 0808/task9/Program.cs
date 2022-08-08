ae8444c11663fbc2ed7c8f505116b1f5d90a4cca
Andrei <apchilap@gmail.com>
2022-08-08 11:00:18 +0300

    + method

--- 0808/task9/Program.cs
+++ 0808/task9/Program.cs
@@ -5,16 +5,26 @@
 // 12-> 2
 // 85 -> 8
 
+int MaxDigit(int num)
+{
+    int firstDigit = num / 10;
+    int secondDigit = num % 10;
+    if (firstDigit == secondDigit) return -1; // если числа равны
+    return firstDigit > secondDigit ? firstDigit : secondDigit;
+}
+
 int number = new Random().Next(10, 100); //до 100 потому что 
 
 Console.WriteLine($"Случайное число из отрезка 10 - 99 -> {number}");
 
-int firstDigit = number / 10;
-int secondDigit = number % 10;
+// int firstDigit = number / 10;
+// int secondDigit = number % 10;
+// if (firstDigit == secondDigit) Console.WriteLine("Числа равны");
+//   {
+//      int max = firstDigit > secondDigit ? firstDigit : secondDigit; //тернарный оператор. Если first больше second, то выводим первое, если нет, второе.
+//      Console.WriteLine($"наибольшая цифра числа {number} -> {max}");
+//   }
 
-if (firstDigit == secondDigit) Console.WriteLine("Числа равны");
-else
-{
-    int max = firstDigit > secondDigit ? firstDigit : secondDigit; //тернарный оператор. Если first больше second, то выводим первое, если нет, второе.
-    Console.WriteLine($"наибольшая цифра числа {number} -> {max}");
-}
+int maxDigit = MaxDigit(number);
+string result = maxDigit != -1 ? maxDigit.ToString() : "Цифры одинаковые";
+Console.WriteLine($"Наибольшая цифра числа {number} -> {result}");
