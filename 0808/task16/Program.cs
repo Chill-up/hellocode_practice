1757355548f44522552c7191ea39fa2472ef4a5e
Andrei <apchilap@gmail.com>
2022-08-08 12:14:42 +0300

    add task16

---
+++ 0808/task16/Program.cs
@@ -0,0 +1,19 @@
+﻿// 14. Напишите программу, которая принимает на
+// вход число и проверяет, кратно ли оно
+// одновременно 7 и 23.
+// 14 -> нет
+// 46 -> нет
+// 161 -> да
+
+Console.WriteLine("Введите первое число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+bool Multiplicity(int num)
+{
+    return num % 7 == 0 && num % 23 == 0;
+}
+
+bool result = Multiplicity(number);
+
+//string res = result ? "да" : "нет" ; можно без переменной сразу в ConsoleWrite это написать
+Console.WriteLine(result ? "да" : "нет");
