a23c3628ca1cc50118b06a0f438de56ce5f7d7e7
Andrei <apchilap@gmail.com>
2022-08-15 11:06:43 +0300

    add task28

---
+++ 1508/task28/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 28: Напишите программу, которая
+// принимает на вход число N и выдаёт
+// произведение чисел от 1 до N.
+// 4 -> 24
+// 5 -> 120
+
+int MultSum(int num)
+{
+    int sum = 1;
+    for (int i = 1; i <= num; i++)
+    {
+        //sum = sum * i;
+        sum *= i;
+    }
+    return sum;
+}
+
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int res = MultSum(number);
+Console.WriteLine($"Сумма чисел от 1 до {number} = {res}");
