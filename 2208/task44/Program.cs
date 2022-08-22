709ac93960c26454f12fac9a3d08d1fef898244b
Andrei <apchilap@gmail.com>
2022-08-22 11:54:56 +0300

    add task44 code

--- 2208/task44/Program.cs
+++ 2208/task44/Program.cs
@@ -4,3 +4,21 @@
 // Если N = 3 -> 0 1 1
 // Если N = 7 -> 0 1 1 2 3 5 8
 
+Console.WriteLine("Введите десятичное число:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+void FirstNFibo(int num)
+{
+    int firstFibo = 0;
+    int secondFibo = 1;
+    Console.Write($"{firstFibo} {secondFibo} ");
+    for (int i = 2; i < num; i++)
+    {
+        int nextFibo = secondFibo + firstFibo;
+        Console.Write($"{nextFibo} ");
+        firstFibo = secondFibo;
+        secondFibo = nextFibo;
+    }
+}
+
+FirstNFibo(number);
