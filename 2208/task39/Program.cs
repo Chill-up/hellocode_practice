1bebb7e872888c642d33d126c34dc9a147288ba4
Andrei <apchilap@gmail.com>
2022-08-22 10:35:16 +0300

    add task39 code

--- 2208/task39/Program.cs
+++ 2208/task39/Program.cs
@@ -1,2 +1,49 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 39: Напишите программу, которая перевернёт
+// одномерный массив (последний элемент будет на первом
+// месте, а первый - на последнем и т.д.)
+// [1 2 3 4 5] -> [5 4 3 2 1]
+// [6 7 3 6] -> [6 3 7 6]
+
+int[] CreateAndFillArrayRandomInt(int size, int min, int max)
+{
+    int[] array = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(min, max + 1);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write(array[i] + ",");
+    }
+    Console.Write(array[array.Length - 1] + "]");
+}
+
+void ReverseArray(int[] array)
+{
+    int size = array.Length;
+    int index1 = 0;
+    int index2 = size - 1;
+    while (index1 < index2)
+    {
+        // стандартная схема замены элементов через временную переменную.
+        int temp = array[index1]; 
+        //создаем переменную temp в цикле, т.к. она больше нигде не используется. Для строк возможно так лучше не делать
+        array[index1] = array[index2];
+        array[index2] = temp;
+        index1++;
+        index2--;
+    }
+}
+
+int[] arr = CreateAndFillArrayRandomInt(8, 1, 10);
+PrintArray(arr);
+Console.WriteLine();
+ReverseArray(arr);
+PrintArray(arr);
