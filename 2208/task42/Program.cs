b646a7e1da3d1499d9ca800d5f673ca2974921de
Andrei <apchilap@gmail.com>
2022-08-22 11:42:05 +0300

    add task42 code

--- 2208/task42/Program.cs
+++ 2208/task42/Program.cs
@@ -3,3 +3,22 @@
 // 45 -> 101101
 // 3 -> 11
 // 2 -> 10
+
+Console.WriteLine("Введите десятичное число:");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int DecToBin(int number)
+{
+    int result = 0;
+    int mult = 1;
+    while (number > 0)
+    {
+        result += number % 2 * mult;
+        number /= 2;
+        mult *= 10;
+    }
+    return result;
+}
+
+int result = DecToBin(num);
+Console.WriteLine(result);
